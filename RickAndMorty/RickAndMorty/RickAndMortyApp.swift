153a87107bea2b09e08c1dbb0b76572f77c70d84
Jorge Santamaría Poveda <jorgesantamariapoveda@gmail.com>
2023-11-17 17:54:44 +0100

    Create project

---
+++ RickAndMorty/RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Jorge on 17/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
